b838a830a115b06d41aa04e09dd79d49e27a19aa
Mio1710 <96384142+Mio1710@users.noreply.github.com>
2024-11-26 17:17:36 +0700

    add: s3 sample

--- src/utils/s3-client.util.ts
+++ src/utils/s3-client.util.ts
@@ -1,10 +1,19 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { Injectable } from '@nestjs/common';
+import {
+  GetObjectAclCommandOutput,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import * as dotenv from 'dotenv';
+import * as stream from 'stream';
 
-const bucketName = process.env.BUCKET_NAME;
-const region = process.env.BUCKET_REGION;
-const accessKeyId = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+// Load environment variables from .env file
+dotenv.config();
+
+const bucketName = process.env.S3_BUCKET_NAME;
+const region = process.env.S3_REGION;
+const accessKeyId = process.env.S3_ACCESS_KEY;
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
 
 const s3 = new S3Client({
   region,
@@ -14,19 +23,25 @@ const s3 = new S3Client({
   },
 });
 
-@Injectable()
-export class S3ClientUtil {
-  constructor() {}
+export async function uploadFile(body, prefix) {
+  const fileName = `${prefix}/${body.file_key}`;
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body: body.file.buffer,
+    ContentType: body.file.mimetype,
+  };
 
-  async uploadFile(body) {
-    const params = {
-      Bucket: bucketName,
-      Key: body.file_path,
-      Body: body.file.buffer,
-      ContentType: body.file.mimetype,
-    };
+  const command = new PutObjectCommand(params);
+  await s3.send(command);
+}
 
-    const command = new PutObjectCommand(params);
-    await s3.send(command);
-  }
+export async function downloadFile(key: string) {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+  };
+
+  const command = new GetObjectCommand(params);
+  return await s3.send(command);
 }
