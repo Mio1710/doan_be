95ef9727e726e9dc73479d194441e383c81cf8c1
Le Nguyen <nguyenthile.work@gmail.com>
2024-12-08 23:05:07 +0700

    Update: create semester

--- src/dtos/semester.dto.ts
+++ src/dtos/semester.dto.ts
@@ -1,11 +1,97 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, Length } from 'class-validator';
 
 export class CreateSemesterDto {
   @IsNotEmpty()
   ten: string;
+
+  @IsOptional()
+  @Length(0, 250, { message: 'Ghi chú không quá 250 ký tự' })
+  note: string;
+
+  @IsNotEmpty()
+  start_date: Date;
+
+  @IsNotEmpty()
+  end_date: Date;
+
+  @IsOptional()
+  start_register_group: Date;
+
+  @IsOptional()
+  end_register_group: Date;
+
+  @IsOptional()
+  start_publish_topic: Date;
+
+  @IsOptional()
+  end_publish_topic: Date;
+
+  @IsOptional()
+  start_register_topic: Date;
+
+  @IsOptional()
+  end_register_topic: Date;
+
+  @IsOptional()
+  start_defense: Date;
+
+  @IsOptional()
+  end_defense: Date;
+
+  @IsOptional()
+  start_report_topic: Date;
+
+  @IsOptional()
+  end_report_topic: Date;
+
+  @IsOptional()
+  public_result: Date;
 }
 
 export class UpdateSemesterDto {
   @IsNotEmpty()
   ten: string;
+
+  @IsOptional()
+  @Length(0, 250, { message: 'Ghi chú không quá 250 ký tự' })
+  note: string;
+
+  @IsNotEmpty()
+  start_date: Date;
+
+  @IsNotEmpty()
+  end_date: Date;
+
+  @IsOptional()
+  start_register_group: Date;
+
+  @IsOptional()
+  end_register_group: Date;
+
+  @IsOptional()
+  start_publish_topic: Date;
+
+  @IsOptional()
+  end_publish_topic: Date;
+
+  @IsOptional()
+  start_register_topic: Date;
+
+  @IsOptional()
+  end_register_topic: Date;
+
+  @IsOptional()
+  start_defense: Date;
+
+  @IsOptional()
+  end_defense: Date;
+
+  @IsOptional()
+  start_report_topic: Date;
+
+  @IsOptional()
+  end_report_topic: Date;
+
+  @IsOptional()
+  public_result: Date;
 }
